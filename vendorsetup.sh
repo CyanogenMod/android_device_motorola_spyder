d514064312cd9c391a3b85017e5a39ab7a0d2db0
dhacker29 <davidhackerdvm@gmail.com>
2011-12-31 00:58:29 -0600

    typo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_targa-userdebug
-add_lunch_combo cm_targa-userdebug
+add_lunch_combo full_spyder-userdebug
+add_lunch_combo cm_spyder-userdebug
