0568df33d483d5f0acda84e46269fede821fe162
dhacker29 <davidhackerdvm@gmail.com>
2012-01-02 02:08:12 -0600

    update init*rc and hijack

--- device_spyder.mk
+++ device_spyder.mk
@@ -138,7 +138,6 @@ PRODUCT_COPY_FILES += \
     out/target/product/spyder/root/sbin/adbd:system/etc/rootfs/sbin/adbd \
     device/motorola/spyder/root/default.prop:system/etc/rootfs/default.prop \
     device/motorola/spyder/root/init.rc:system/etc/rootfs/init.rc \
-    device/motorola/spyder/root/init.spyder.usb.rc:system/etc/rootfs/init.spyder.usb.rc \
     device/motorola/spyder/root/init.mapphone_cdma.rc:system/etc/rootfs/init.mapphone_cdma.rc \
     device/motorola/spyder/root/init.mapphone_umts.rc:system/etc/rootfs/init.mapphone_umts.rc \
     device/motorola/spyder/root/usbcheck.sh:system/etc/rootfs/usbcheck.sh \
@@ -148,9 +147,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/motorola/spyder/root/default.prop:root/default.prop \
     device/motorola/spyder/root/init.rc:root/init.rc \
-    device/motorola/spyder/root/init.spyder.usb.rc:root/init.spyder.usb.rc \
     device/motorola/spyder/root-hijack/init.mapphone_cdma.rc:root/init.mapphone_cdma.rc \
-    device/motorola/spyder/root-hijack/init.mapphone_umts.rc:system/etc/root/init.mapphone_umts.rc \
+    device/motorola/spyder/root-hijack/init.mapphone_umts.rc:root/init.mapphone_umts.rc \
     device/motorola/spyder/root/usbcheck.sh:root/usbcheck.sh \
     device/motorola/spyder/root/ueventd.rc:root/ueventd.rc \
 
