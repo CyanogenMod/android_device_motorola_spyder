91ac240b686fe4355597606bf0031f39bf6a050c
dhacker29 <davidhackerdvm@gmail.com>
2012-02-06 17:59:20 -0600

    add camera to build and switch to moto IMS instead of samsung

--- device_spyder.mk
+++ device_spyder.mk
@@ -37,8 +37,8 @@ PRODUCT_COPY_FILES += \
 #    lights.spyder \
 #    sensors.spyder \
 
-#PRODUCT_PACKAGES += \
-#    camera.omap4
+PRODUCT_PACKAGES += \
+    camera.omap4
 
 PRODUCT_PACKAGES += \
     audio.primary.spyder \
@@ -254,10 +254,11 @@ PRODUCT_COPY_FILES += \
 
 # stuff specific to ti OMAP4 hardware
 $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
-#$(call inherit-product, hardware/ti/camera/camera.mk)
+$(call inherit-product, hardware/ti/camera/camera.mk)
 $(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
 #$(call inherit-product-if-exists, vendor/google/google-vendor.mk)
-$(call inherit-product-if-exists, vendor/verizon/verizon-vendor.mk)
+$(call inherit-product-if-exists, vendor/verizon/spyder-verizon-vendor.mk)
+
 
 $(call inherit-product-if-exists, vendor/motorola/spyder/spyder-vendor.mk)
 
