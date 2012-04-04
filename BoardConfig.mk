fc2948e5b8a507e0f95676545552f023847b04c8
dhacker29 <davidhackerdvm@gmail.com>
2012-04-04 00:27:34 -0500

    make some changes to usb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,8 +34,8 @@ BOARD_PAGE_SIZE := 0x4096
 
 
 # Storage / Sharing
-BOARD_VOLD_MAX_PARTITIONS := 96
-#BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+#BOARD_VOLD_MAX_PARTITIONS := 96
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 #TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun%d/file"
 #BOARD_CUSTOM_USB_CONTROLLER := ../../device/motorola/spyder/UsbController.cpp
 
