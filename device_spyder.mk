508125bcf1446514998f85d1c5d0f1b72236b42c
dhacker29 <davidhackerdvm@gmail.com>
2012-05-25 03:18:50 -0500

    forgot to update system/usr files

--- device_spyder.mk
+++ device_spyder.mk
@@ -87,7 +87,6 @@ PRODUCT_PACKAGES += \
     spyder_releaseutils-tune2fs
 
 PRODUCT_PACKAGES += \
-    CameraOMAP4 \
     camera_test \
     Superuser \
     su \
@@ -153,46 +152,32 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 
 
-#    vendor/motorola/spyder/proprietary/etc/permissions/platform.xml:system/etc/permissions/platform.xml \
 # Prebuilts
 PRODUCT_COPY_FILES += \
-    device/motorola/spyder/prebuilt/bin/battd:system/bin/battd \
-    device/motorola/spyder/prebuilt/bin/hijack:system/bin/hijack \
-    device/motorola/spyder/prebuilt/bin/hijack.log_dump:system/bin/hijack.log_dump \
-    device/motorola/spyder/prebuilt/bin/mount_ext3.sh:system/bin/mount_ext3.sh \
-    device/motorola/spyder/prebuilt/bin/strace:system/bin/strace \
-    device/motorola/spyder/prebuilt/etc/gps.conf:system/etc/gps.conf \
-    device/motorola/spyder/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
-    device/motorola/spyder/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
-    device/motorola/spyder/prebuilt/etc/hijack-boot.zip:system/etc/hijack-boot.zip \
-    device/motorola/spyder/prebuilt/usr/idc/evfwd.idc:system/usr/idc/evfwd.idc \
-    device/motorola/spyder/prebuilt/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
-    device/motorola/spyder/prebuilt/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
-    device/motorola/spyder/prebuilt/usr/keychars/evfwd.kcm:system/usr/keychars/evfwd.kcm \
-    device/motorola/spyder/prebuilt/usr/keychars/omap4-keypad.kcm:system/usr/keychars/omap4-keypad.kcm \
-    device/motorola/spyder/prebuilt/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
-    device/motorola/spyder/prebuilt/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
-    device/motorola/spyder/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/motorola/spyder/prebuilt/usr/keylayout/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
-    device/motorola/spyder/prebuilt/usr/keylayout/cy8c201xx.kl:system/usr/keylayout/cy8c201xx.kl \
-    device/motorola/spyder/prebuilt/usr/keylayout/evfwd.kl:system/usr/keylayout/evfwd.kl \
-    device/motorola/spyder/prebuilt/usr/keylayout/omap4-keypad.kl:system/usr/keylayout/omap4-keypad.kl \
-    device/motorola/spyder/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    
-#    device/motorola/spyder/prebuilt/usr/keylayout/atmxt-i2c.kl:system/usr/keylayout/atmxt-i2c.kl \
-#    device/motorola/spyder/prebuilt/usr/keylayout/light-prox.kl:system/usr/keylayout/light-prox.kl \
-#    device/motorola/spyder/prebuilt/usr/keylayout/mapphone-switch.kl:system/usr/keylayout/mapphone-switch.kl \
-#    device/motorola/spyder/prebuilt/usr/idc/cpcap-key.idc:system/usr/idc/cpcap-key.idc \
-#    device/motorola/spyder/prebuilt/usr/idc/cy8c201xx.idc:system/usr/idc/cy8c201xx.idc \
-#    device/motorola/spyder/prebuilt/usr/idc/light-prox.idc:system/usr/idc/light-prox.idc \
-#    device/motorola/spyder/prebuilt/usr/idc/mapphone-switch.idc:system/usr/idc/mapphone-switch.idc \
-#    device/motorola/spyder/prebuilt/usr/idc/omap4-keypad.idc:system/usr/idc/omap4-keypad.idc \
-#    device/motorola/spyder/prebuilt/usr/idc/atmxt-i2c.idc:system/usr/idc/atmxt-i2c.idc \
-#    device/motorola/spyder/prebuilt/usr/keychars/cpcap-key.kcm:system/usr/keychars/cpcap-key.kcm \
-#    device/motorola/spyder/prebuilt/usr/keychars/cy8c201xx.kcm:system/usr/keychars/cy8c201xx.kcm \
-#    device/motorola/spyder/prebuilt/usr/keychars/light-prox.kcm:system/usr/keychars/light-prox.kcm \
-#    device/motorola/spyder/prebuilt/usr/keychars/mapphone-switch.kcm:system/usr/keychars/mapphone-switch.kcm \
-#    device/motorola/spyder/prebuilt/usr/keychars/atmxt-i2c.kcm:system/usr/keychars/atmxt-i2c.kcm \
+    device/moto/spyder/prebuilt/bin/battd:system/bin/battd \
+    device/moto/spyder/prebuilt/bin/hijack:system/bin/hijack \
+    device/moto/spyder/prebuilt/bin/hijack.log_dump:system/bin/hijack.log_dump \
+    device/moto/spyder/prebuilt/bin/mount_ext3.sh:system/bin/mount_ext3.sh \
+    device/moto/spyder/prebuilt/bin/strace:system/bin/strace \
+    device/moto/spyder/prebuilt/etc/gps.conf:system/etc/gps.conf \
+    device/moto/spyder/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    device/moto/spyder/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
+    device/moto/spyder/prebuilt/etc/hijack-boot.zip:system/etc/hijack-boot.zip \
+    device/moto/spyder/prebuilt/usr/idc/evfwd.idc:system/usr/idc/evfwd.idc \
+    device/moto/spyder/prebuilt/usr/idc/Motorola_Mobility_Motorola_HD_Dock.idc:system/usr/idc/Motorola_Mobility_Motorola_HD_Dock.idc \
+    device/moto/spyder/prebuilt/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
+    device/moto/spyder/prebuilt/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
+    device/moto/spyder/prebuilt/usr/keychars/evfwd.kcm:system/usr/keychars/evfwd.kcm \
+    device/moto/spyder/prebuilt/usr/keychars/omap4-keypad.kcm:system/usr/keychars/omap4-keypad.kcm \
+    device/moto/spyder/prebuilt/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
+    device/moto/spyder/prebuilt/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
+    device/moto/spyder/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/moto/spyder/prebuilt/usr/keylayout/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
+    device/moto/spyder/prebuilt/usr/keylayout/cy8c201xx.kl:system/usr/keylayout/cy8c201xx.kl \
+    device/moto/spyder/prebuilt/usr/keylayout/evfwd.kl:system/usr/keylayout/evfwd.kl \
+    device/moto/spyder/prebuilt/usr/keylayout/omap4-keypad.kl:system/usr/keylayout/omap4-keypad.kl \
+    device/moto/spyder/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+
 
 # Phone settings
 PRODUCT_COPY_FILES += \
