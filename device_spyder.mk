ed5291ecd1a86c2f76d188074b0152faebfc38da
David Hacker <davidhackerdvm@gmail.com>
2012-06-04 16:48:23 -0500

    use BUILD_PREBUILT for wifi tether

--- device_spyder.mk
+++ device_spyder.mk
@@ -101,8 +101,8 @@ PRODUCT_PACKAGES += \
     Apollo \
 
 # WirelessTether
+PRODUCT_PACKAGES += wifi_tether_v3_1-beta14
 PRODUCT_COPY_FILES += \
-    device/motorola/spyder/prebuilt/app/wifi_tether_v3_1-beta14.apk:system/app/wifi_tether_v3_1-beta14.apk \
     device/motorola/spyder/prebuilt/lib/libwtnativetask.so:system/lib/libwtnativetask.so \
     
 
