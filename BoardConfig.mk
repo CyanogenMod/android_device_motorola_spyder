ca99007063604df271dc1432572a067b381534f9
David Hacker <davidhackerdvm@gmail.com>
2012-08-16 22:27:28 -0500

    switch to TI wpa_supplicant
    Update WPA_SUPPLICANT_VERSION to point to TI wpa_supplicant tree.
    Also TI tree does not need private libraries.
    
    If we want to switch back to Google wpa_supplicant, then we need
    to define BOARD_WPA_SUPPLICANT_PRIVATE_LIB and BOARD_HOSTAPD_PRIVATE_LIB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,11 +66,9 @@ BOARD_MTP_DEVICE := "/dev/mtp"
 USES_TI_MAC80211 := true
 ifdef USES_TI_MAC80211
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 BOARD_HOSTAPD_DRIVER             := NL80211
 PRODUCT_WIRELESS_TOOLS           := true
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
