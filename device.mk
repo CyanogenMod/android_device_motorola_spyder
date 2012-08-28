1d336c66bb4600e56a12887c3458d5ff9062f94c
dhacker29 <davidhackerdvm@gmail.com>
2012-08-28 05:10:35 -0500

    move common items to device/motorola/common

--- device.mk
+++ device.mk
@@ -4,21 +4,8 @@
 
 DEVICE_FOLDER := device/motorola/spyder
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
-
 # Device overlay
     DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay/aosp
-
-# high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-PRODUCT_PACKAGES := \
-    charger \
-    charger_res_images
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -30,79 +17,13 @@ PRODUCT_COPY_FILES += \
 
 # Hardware HALs
 PRODUCT_PACKAGES += \
-    camera.omap4 \
-    libinvensense_mpl \
-    hwcomposer.spyder \
-    libedid \
-
-PRODUCT_PACKAGES += \
-    libaudioutils \
-    libaudiohw_legacy \
-
-# BlueZ a2dp Audio HAL module
-PRODUCT_PACKAGES += \
-    audio.a2dp.default
-
-# BlueZ test tools
-PRODUCT_PACKAGES += \
-    hciconfig \
-    hcitool
+    hwcomposer.spyder
 
 # Modem
 PRODUCT_PACKAGES += \
     nc \
     Stk \
-    libaudiomodemgeneric \
-    libreference-cdma-sms \
-    rild \
-    radiooptions
-
-# Wifi
-PRODUCT_PACKAGES += \
-    lib_driver_cmd_wl12xx \
-    dhcpcd.conf \
-    hostapd.conf \
-    wifical.sh \
-    wpa_supplicant.conf \
-    TQS_D_1.7.ini \
-    TQS_D_1.7_127x.ini \
-    crda \
-    regulatory.bin \
-    calibrator \
-    busybox \
-
-# Wifi Direct and WPAN
-PRODUCT_PACKAGES += \
-    ti_wfd_libs \
-    ti-wpan-fw
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    bt_sco_app \
-    uim-sysfs 
-
-# Release utilities
-PRODUCT_PACKAGES += \
-    spyder_releaseutils-check_kernel \
-    spyder_releaseutils-finalize_release \
-    spyder_releaseutils-mke2fs \
-    spyder_releaseutils-tune2fs
-
-PRODUCT_PACKAGES += \
-    evtest \
-    camera_test \
-    Camera \
-    CameraOMAP4 \
-    Superuser \
-    su \
-    DockAudio \
-    parse_hdmi_edid \
-
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory \
-    FileManager \
-    MusicFX \
-    Apollo \
+    libreference-cdma-sms
 
 # Rootfs files
 PRODUCT_COPY_FILES += \
@@ -112,71 +33,32 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/root/init.mapphone_umts.rc:/root/init.mapphone_umts.rc \
     $(DEVICE_FOLDER)/root/ueventd.rc:/root/ueventd.rc \
     $(DEVICE_FOLDER)/root/ueventd.mapphone_cdma.rc:/root/ueventd.mapphone_cdma.rc \
-    $(DEVICE_FOLDER)/root/ueventd.mapphone_umts.rc:/root/ueventd.mapphone_umts.rc \
+    $(DEVICE_FOLDER)/root/ueventd.mapphone_umts.rc:/root/ueventd.mapphone_umts.rc
 
 # Kexec files
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/kexec/arm_kexec.ko:system/etc/kexec/arm_kexec.ko \
-    $(DEVICE_FOLDER)/kexec/atags:system/etc/kexec/atags \
     $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
-    $(DEVICE_FOLDER)/kexec/kexec:system/etc/kexec/kexec \
-    $(DEVICE_FOLDER)/kexec/kexec.ko:system/etc/kexec/kexec.ko \
-    $(DEVICE_FOLDER)/kexec/uart.ko:system/etc/kexec/uart.ko \
-    out/target/product/spyder/ramdisk.img:system/etc/kexec/ramdisk.img \
-    out/target/product/spyder/kernel:system/etc/kexec/kernel \
-
-# Permissions files
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    out/target/product/umts_spyder/ramdisk.img:system/etc/kexec/ramdisk.img \
+    out/target/product/umts_spyder/kernel:system/etc/kexec/kernel
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/prebuilt/bin/battd:system/bin/battd \
     $(DEVICE_FOLDER)/prebuilt/bin/mount_ext3.sh:system/bin/mount_ext3.sh \
-    $(DEVICE_FOLDER)/prebuilt/bin/strace:system/bin/strace \
     $(DEVICE_FOLDER)/prebuilt/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin \
     $(DEVICE_FOLDER)/prebuilt/etc/gps.conf:system/etc/gps.conf \
     $(DEVICE_FOLDER)/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
     $(DEVICE_FOLDER)/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
     $(DEVICE_FOLDER)/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
-    $(DEVICE_FOLDER)/prebuilt/etc/TICameraCameraProperties.xml:system/etc/TICameraCameraProperties.xml \
+    $(DEVICE_FOLDER)/prebuilt/etc/TICameraCameraProperties.xml:system/etc/TICameraCameraProperties.xml
 
 #    $(DEVICE_FOLDER)/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
 
 # Phone settings
 PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
-    $(DEVICE_FOLDER)/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml \
+    device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml
 
-# we have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-# still need to set english for audio init
-PRODUCT_LOCALES += en_US
-
-
-# stuff specific to ti OMAP4 hardware
-$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
-$(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
-$(call inherit-product, device/ti/proprietary-open/wl12xx/wlan/wl12xx-wlan-fw-products.mk)
-$(call inherit-product-if-exists, vendor/motorola/common/proprietary/imgtec/sgx-imgtec-bins.mk)
-$(call inherit-product-if-exists, vendor/motorola/common/proprietary/custom-omap4xxx/custom-omap4.mk)
+$(call inherit-product, device/motorola/common/common.mk)
 $(call inherit-product-if-exists, vendor/motorola/common/proprietary/apps/verizon.mk)
 $(call inherit-product-if-exists, vendor/motorola/spyder/spyder-vendor.mk)
 
