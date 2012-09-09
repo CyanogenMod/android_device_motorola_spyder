91c36e94f454323ef75c01d2fe2268979fcc7700
dhacker29 <davidhackerdvm@gmail.com>
2012-09-09 13:38:14 -0500

    Allow kexec and stock-based to build from same device/vendor

--- full_spyder.mk
+++ full_spyder.mk
@@ -35,6 +35,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 # Inherit from spyder device
+$(call inherit-product-if-exists, device/motorola/kexec/kexec.mk)
 $(call inherit-product, device/motorola/spyder/device.mk)
 
 # Set those variables here to overwrite the inherited values.
