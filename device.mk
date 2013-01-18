6eab6bbc8220e456e5a09647fe35414de708d1b2
dhacker29 <davidhackerdvm@gmail.com>
2013-01-17 18:44:52 -0600

    Build libasound and conf from source

--- device.mk
+++ device.mk
@@ -19,8 +19,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/audio/alsa.omap4.so:/system/lib/hw/alsa.omap4.so \
     $(DEVICE_FOLDER)/audio/audio.primary.omap4.so:/system/lib/hw/audio.primary.spyder.so \
     $(DEVICE_FOLDER)/audio/audio_policy.omap4.so:/system/lib/hw/audio_policy.omap4.so \
-    $(DEVICE_FOLDER)/audio/libasound.so:/system/lib/libasound.so \
     $(DEVICE_FOLDER)/audio/libaudio_ext.so:/system/lib/libaudio_ext.so
+
+PRODUCT_PACKAGES += \
+    libasound
 
 # Modem
 PRODUCT_PACKAGES += \
