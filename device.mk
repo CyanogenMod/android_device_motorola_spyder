5a089e6fb0317067139d91d58955e0f41ad5c707
dhacker29 <davidhackerdvm@gmail.com>
2013-03-09 16:18:09 -0600

    Setup for kexec
    Change-Id: Ic12d0e24c75b9755989f68d4f401abaa1fed0218

--- device.mk
+++ device.mk
@@ -35,20 +35,18 @@ PRODUCT_PACKAGES += \
     Stk \
     libreference-cdma-sms
 
+# Kexec files
+PRODUCT_COPY_FILES += \
+    $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
+    $(OUT)/ramdisk.img:system/etc/kexec/ramdisk.img \
+    $(OUT)/kernel:system/etc/kexec/kernel
+
 # Prebuilts
 PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
     $(DEVICE_FOLDER)/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
     $(DEVICE_FOLDER)/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
     $(DEVICE_FOLDER)/prebuilt/etc/vold.fstab:system/etc/vold.fstab
-
-# copy all kernel modules under the "modules" directory to system/lib/modules
-ifneq ($(BOARD_USES_KEXEC),true)
-PRODUCT_COPY_FILES += $(shell \
-    find device/motorola/spyder/modules -name '*.ko' \
-    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
-    | tr '\n' ' ')
-endif
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/omap4-common/proprietary/vzw/verizon.mk)
