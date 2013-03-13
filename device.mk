be881593235159f81ee51c5737991e500c9a8b16
dhacker29 <davidhackerdvm@gmail.com>
2013-03-12 22:42:48 -0500

    Setup init.safestrap for symlinks and move common files
    Change-Id: Ia49b44eb84a1ca79a889920b9565af011618e0ce

--- device.mk
+++ device.mk
@@ -37,16 +37,15 @@ PRODUCT_PACKAGES += \
 
 # Kexec files
 PRODUCT_COPY_FILES += \
+    $(DEVICE_FOLDER)/init.safestrap.rc/root/init.safestrap.rc \
     $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
     $(OUT)/ramdisk.img:system/etc/kexec/ramdisk.img \
     $(OUT)/kernel:system/etc/kexec/kernel
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    $(DEVICE_FOLDER)/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
-    $(DEVICE_FOLDER)/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    $(DEVICE_FOLDER)/prebuilt/etc/vold.fstab:system/etc/vold.fstab
+    $(DEVICE_FOLDER)/media_profiles.xml:system/etc/media_profiles.xml \
+    $(DEVICE_FOLDER)/vold.fstab:system/etc/vold.fstab
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/omap4-common/proprietary/vzw/verizon.mk)
