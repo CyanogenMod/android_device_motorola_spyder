518b39662159437b036b9fb6e6946c85990b9c74
dhacker29 <davidhackerdvm@gmail.com>
2013-05-31 05:15:25 -0500

    Use shared defconfig
    Change-Id: I1f8ae7bfedf5b69af232be6eba5b15126bf3413c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,3 @@ TARGET_BOOTLOADER_BOARD_NAME := spyder
 BOARD_KERNEL_CMDLINE := root=/dev/ram0 rw mem=1023M@0x80000000 console=null vram=10300K omapfb.vram=0:8256K,1:4K,2:2040K init=/init ip=off mmcparts=mmcblk1:p7(pds),p8(utags),p14(boot),p15(recovery),p16(cdrom),p17(misc),p18(cid),p19(kpanic),p20(system),p21(cache),p22(preinstall),p23(webtop),p24(userdata),p25(emstorage) mot_sst=1 androidboot.bootloader=0x0A74
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_PAGE_SIZE := 0x4096
-
-# Kernel Build
-TARGET_KERNEL_SOURCE := kernel/motorola/omap4-common
-TARGET_KERNEL_CONFIG := mapphone_spyder_defconfig
