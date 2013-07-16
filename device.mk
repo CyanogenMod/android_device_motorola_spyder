fc9e823388adbf194e9c8a2e3931d682cd851c1c
dhacker29 <davidhackerdvm@gmail.com>
2013-07-15 23:36:12 -0500

    Add extract-files
    Change-Id: Iac54c2634219597cf57fe3a84943071fe653789d

--- device.mk
+++ device.mk
@@ -49,5 +49,5 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/spyder/spyder-vendor.mk)
-$(call inherit-product-if-exists, vendor/motorola/omap4-common/common-vendor-pvr.mk)
-
+$(call inherit-product-if-exists, vendor/motorola/omap4-common/omap4-common-imgtec-vendor-blobs.mk)
+$(call inherit-product-if-exists, vendor/motorola/omap4-common/omap4-common-lte-vendor-blobs.mk)
---
+++ extract-files.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+export VENDOR=motorola
+export DEVICE=spyder
+
+../omap4-common/extract-files.sh $@
