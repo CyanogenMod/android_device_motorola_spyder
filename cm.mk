8a4e81d5d9660a71a6598df91d3cb5a113b7c219
dhacker29 <davidhackerdvm@gmail.com>
2013-10-29 23:02:39 -0500

    Remove cdma.mk
    Change-Id: I186bb0891d34d343127a7eba5356d3671083f540

--- cm.mk
+++ cm.mk
@@ -4,9 +4,6 @@ $(call inherit-product, device/motorola/spyder/full_spyder.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit some common CDMA stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
 #
 # Setup device specific product configuration.
 #
